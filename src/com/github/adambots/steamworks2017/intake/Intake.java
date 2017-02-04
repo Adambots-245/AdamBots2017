814b1b5b0d333b55b2a8691f53dd83183f52109f
Ethan Rylko <ethan.rylko@gmail.com>
2017-02-03 20:52:34 -0500

    Moved STOP constant to Constants class and renamed it to MOTOR_STOP.

--- src/com/github/adambots/steamworks2017/intake/Intake.java
+++ src/com/github/adambots/steamworks2017/intake/Intake.java
@@ -1,6 +1,7 @@
 package com.github.adambots.steamworks2017.intake;
 
 import org.usfirst.frc.team245.robot.Actuators;
+import org.usfirst.frc.team245.robot.Constants;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Intake {
@@ -8,7 +9,6 @@ public class Intake {
 	//TODO: Set MAX_CURRENT to a number once we get an exact value
 	public static double MAX_CURRENT;
 	public static double SPEED = 0.8;
-	public static double STOP = 0;
 	
 	/*
 	 * Runs intakeMotor
@@ -18,7 +18,7 @@ public class Intake {
 		if (intakeButton){
 			Actuators.getIntakeMotor().set(SPEED);
 		} else {
-			Actuators.getIntakeMotor().set(STOP);
+			Actuators.getIntakeMotor().set(Constants.MOTOR_STOP);
 		}
 		currentCheck();
 	}
--- src/org/usfirst/frc/team245/robot/Constants.java
+++ src/org/usfirst/frc/team245/robot/Constants.java
@@ -5,6 +5,9 @@ public class Constants {
 	public static final double MAX_MOTOR_SPEED = 1;
 	public static final double MIN_MOTOR_SPEED = -1;
 	
+	//Stops motor
+	public static final double MOTOR_STOP = 0;
+	
 	//Port map
 	public static final int LEFT_FRONT_DRIVE_MOTOR_PORT = 0;
 	public static final int RIGHT_FRONT_DRIVE_MOTOR_PORT = 1;
