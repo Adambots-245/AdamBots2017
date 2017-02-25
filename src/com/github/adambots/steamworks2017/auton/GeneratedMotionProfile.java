1b5c2b3cc3f49919f3fad9c8f4df2b4c18bcad47
Ethan Rylko <ethan.rylko@gmail.com>
2017-02-25 14:51:39 -0500

    Added MotionProfileReader, added Apache POI, updated a few other MotionProfile-related classes.

---
+++ src/com/github/adambots/steamworks2017/auton/GeneratedMotionProfile.java
@@ -0,0 +1,8 @@
+package com.github.adambots.steamworks2017.auton;
+
+public class GeneratedMotionProfile {			
+	public static final int kNumPoints = 185;		
+	// Position(rotations)		Velocity(RPM)		Duration(ms)
+	public static double [][]Points = new double[][]{};
+	
+}
